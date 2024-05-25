2a0d46755ecf11e279aca75e6e8e7703a95f487b
Pierino Bonanni <pbonanni@nycap.rr.com>
2024-05-25 00:46:28 -0400

    NanFillDataset: added 'Ts' option.

--- NanFillDataset.m
+++ NanFillDataset.m
@@ -1,19 +1,25 @@
-function Data = NanFillDataset(Data,Trange)
+function Data = NanFillDataset(Data,varargin)
 
 % NANFILLDATASET - Fill sampling holes in a dataset.
 % Data1 = NanFillDataset(Data)
+% Data1 = NanFillDataset(Data,Ts,Trange)
 % Data1 = NanFillDataset(Data,Trange)
+% Data1 = NanFillDataset(Data,Ts)
 %
-% Performs nan-filling operation on a dataset with missing time 
-% samples. Determines the dominant sample time, then fills missing 
-% samples with NaN values, resulting in a dataset with uniform time 
-% sampling. Optional 'Trange' is a 1x2 vector specifying the desired 
-% time range in compatible time units, defaulting to the original 
-% dataset time range if not provided. 
+% Performs time-grid regularization and nan-filling on a dataset to 
+% repair irregularly placed and/or missing time samples. Determines 
+% the dominant sample time, or explicit sample time 'Ts' if provided, 
+% then removes samples deviating from the grid and fills missing samples 
+% with NaN values. The result is a dataset with uniform time sampling. 
+% Optional 'Trange' is a 1x2 vector specifying the desired time range 
+% in compatible time units, defaulting to the original dataset time 
+% range if not provided. 
 %
 % Note: For best results, all time values should be evenly divisible 
-% by the dominant sample time. On datasets with absolute time units, 
-% all time values should have whole-number seconds. 
+% by the dominant or specified sample time. On datasets with absolute 
+% time units, time values in a vector 't' can be rounded to whole-number 
+% seconds using "dateshift(t,'start','sec','nearest')", and rounding to 
+% minutes, hours, etc. follows similarly. 
 %
 % P.G. Bonanni
 % 8/9/23
@@ -22,8 +28,20 @@ function Data = NanFillDataset(Data,Trange)
 % Distributed under GNU General Public License v2.0.
 
 
-if nargin < 2
-  Trange = [];
+args = varargin;  % initialize
+if length(args)>2
+  error('Invalid usage.')
+elseif length(args)==1
+  Ts = args{1};
+  if numel(Ts)<2
+    Trange = [];
+  elseif numel(Ts)==2 && size(Ts,1)==1
+    Trange = Ts;
+    Ts = [];
+  end
+elseif length(args)==2
+  Ts     = args{1};
+  Trange = args{2};
 end
 
 % Check 'Data' argument
@@ -35,28 +53,43 @@ if ~flag || ~valid
   error('Input ''Data'' is not a valid dataset.  See "IsDataset".')
 end
 
+% Check time vector
+if any(diff(Data.Time.Values) < 0)
+  error('Time vector is invalid: reversals detected.')
+elseif any(diff(Data.Time.Values) == 0)
+  error('Time vector is invalid: repeated time points detected.')
+end
+
 % Default time range
-if isempty(Trange)
+if isnumeric(Trange) && isempty(Trange)
   ti = Data.Time.Values(1);
   tf = Data.Time.Values(end);
   Trange = [ti,tf];
 end
 
+% Check 'Ts'
+if ~isnumeric(Ts) || numel(Ts)>1
+  error('Specified ''Ts'' is invalid.')
+end
+
 % Check 'Trange' argument
-if ~strcmp(class(Data.Time.Values),class(Trange))
-  error('Specified ''Trange'' is of incompatible type.')
-elseif ~isrow(Trange) || numel(Trange)~=2
+if ~isrow(Trange) || numel(Trange)~=2 || Trange(2)<=Trange(1)
   error('Invalid ''Trange'' parameter.')
+elseif ~strcmp(class(Data.Time.Values),class(Trange))
+  error('Specified ''Trange'' is invalid or of incompatible type.')
 end
 
 % Limit time range as necessary
 Data = LimitTimeRange(Data,Trange);
 
-% Determine dominant sampling time
-if isdatetime(Data.Time.Values)
-  Ts = round(GetSampleTime(Data,'mode'));
-else  % if real-valued time vector
-  Ts = GetSampleTime(Data,'mode');
+% If 'Ts' not provided
+if isempty(Ts)
+  % Determine dominant sampling time
+  if isdatetime(Data.Time.Values)
+    Ts = round(GetSampleTime(Data,'mode'));
+  else  % if real-valued time vector
+    Ts = GetSampleTime(Data,'mode');
+  end
 end
 
 % Compute sampling schedule
